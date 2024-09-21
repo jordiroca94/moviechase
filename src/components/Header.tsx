f4c53999ed2272f24fe5802f0f2cd387469a927f
Jordi Roca Soler <jordirocasoler94@gmail.com>
2024-09-21 15:56:00 +0200

    feat:tmdb border animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,26 +23,28 @@ const Header = () => {
   }, [query]);
 
   return (
-    <header className="flex items-center justify-between py-4 px-4 lg:px-8 bg-primary">
-      <Link href="/">
-        <h1 className="text-3xl">Moviechase</h1>
-      </Link>
-      <div className="flex items-center gap-8">
-        <nav className="flex gap-4">
-          {links.map((item) => (
-            <Link className="text-base" key={item.label} href={item.link}>
-              {item.label}
-            </Link>
-          ))}
-        </nav>
-        <div className="relative">
-          <input
-            type="text"
-            onChange={(e) => setQuery(e.target.value)}
-            placeholder="Search..."
-            className="bg-white border border-gray-300 rounded-lg px-4 py-1.5 focus:outline-none focus:ring-2 focus:ring-secondary text-black pr-10"
-          />
-          <CiSearch className="absolute text-black top-1.5 right-2 size-7" />
+    <header className="md:block hidden">
+      <div className="flex items-center justify-between py-4 px-4 lg:px-8 bg-primary">
+        <Link href="/">
+          <h1 className="text-3xl">Moviechase</h1>
+        </Link>
+        <div className="flex items-center gap-8">
+          <nav className="flex gap-4">
+            {links.map((item) => (
+              <Link className="text-base" key={item.label} href={item.link}>
+                {item.label}
+              </Link>
+            ))}
+          </nav>
+          <div className="relative">
+            <input
+              type="text"
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Search..."
+              className="bg-white border border-gray-300 rounded-lg px-4 py-1.5 focus:outline-none focus:ring-2 focus:ring-secondary text-black pr-10"
+            />
+            <CiSearch className="absolute text-black top-1.5 right-2 size-7" />
+          </div>
         </div>
       </div>
     </header>
