f38e2e4198b3b4d0447957874490b0db20be8b68
Jordi Roca Soler <jordirocasoler94@gmail.com>
2024-09-30 13:18:42 +0200

    fix: h1 mobile fontsize

--- src/components/ui/H1Title.tsx
+++ src/components/ui/H1Title.tsx
@@ -5,6 +5,10 @@ const H1Title = ({
   children: React.ReactNode;
   className?: string;
 }) => {
-  return <h1 className={`text-5xl ${className && className}`}>{children}</h1>;
+  return (
+    <h1 className={`text-3xl sm:text-5xl ${className && className}`}>
+      {children}
+    </h1>
+  );
 };
 export default H1Title;
