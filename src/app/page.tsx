2451750d7df105374a53115286cd5f28445b2fea
Jordi <jordirocasoler94@gmail.com>
2024-10-07 22:22:30 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,11 @@ import HomeMovies from "@/components/HomeMovies";
 import HomeShows from "@/components/HomeShows";
 import Header from "@/components/Header";
 import Tmdb from "@/components/Tmdb";
-import VideoHero from "@/components/VideoHero";
 
 export default function Home() {
   return (
     <main>
       <Header />
-      {/* <VideoHero /> */}
       <HomeMovies />
       <HomeShows />
       <Tmdb />
