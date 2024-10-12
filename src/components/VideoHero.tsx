b3b91e7cd92da6139da981f5543122599755a3b1
Jordi <jordirocasoler94@gmail.com>
2024-10-12 15:53:34 +0200

    feat: HeroVideo

--- src/components/VideoHero.tsx
+++ src/components/VideoHero.tsx
@@ -1,7 +1,14 @@
 const VideoHero = () => {
   return (
-    <div>
-      <video src="./heroVideo.mp4"></video>
+    <div className="h-[90vh]">
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/fiqqAI0e4Nc?si=b_Le1MizZFTqGQL-&autoplay=1&mute=1"
+        title="Joker 2"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 };
