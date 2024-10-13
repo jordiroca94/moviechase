527b2b6459bfc600c21afc169f11d38340488b5f
Jordi <jordirocasoler94@gmail.com>
2024-10-13 14:19:01 +0200

    fix: height

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,13 @@
 import Header from "@/components/Header";
 export default function NotFound() {
   return (
-    <main>
+    <main className="h-screen flex flex-col">
       <Header />
-      <div className="grid place-items-center h-screen">
-        <div className="flex justify-center items-center gap-2 divide-x-2">
-          <p className="text-lg sm:text-3xl pr-3 sm:pr-6">404</p>
-          <h2 className="text-base sm:text-lg pl-3 sm:pl-6 text-center">
-            This page could not be found.
-          </h2>
-        </div>
+      <div className="flex justify-center items-center gap-2 divide-x-2 flex-grow">
+        <p className="text-lg sm:text-3xl pr-3 sm:pr-6">404</p>
+        <h2 className="text-base sm:text-lg pl-3 sm:pl-6 text-center">
+          This page could not be found.
+        </h2>
       </div>
     </main>
   );
