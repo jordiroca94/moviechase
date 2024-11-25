cfc90dfc21b2e17017c5ce3a9022e42865b38883
Jordi Roca <jordi.roca@acaitravel.com>
2024-11-25 22:23:13 +0100

    fix: profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,6 +2,9 @@
 import { redirect, useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import Container from "./ui/Container";
+import Image from "next/image";
+import PersonPlaceholder from "../../public/images/profilePlaceholder.png";
+import Grid from "./ui/Grid";
 
 const Profile = () => {
   const router = useRouter();
@@ -26,9 +29,33 @@ const Profile = () => {
             Log out
           </button>
         </div>
-        <div className="flex justify-center my-20 ">
-          THIS IS THE PROFILE PAGE WIP
-        </div>
+        <Grid className="my-20">
+          <Image
+            src={PersonPlaceholder}
+            alt="Profile"
+            className="rounded-full lg:col-span-2 lg:col-start-5 col-span-4 sm:col-start-3 aspect-square"
+          />
+          <div className="hidden lg:block lg:col-span-2">
+            <div className="flex text-base">
+              <span className="font-semibold">Name:&nbsp;</span>
+              <p>John</p>
+            </div>
+            <div className="flex text-base">
+              <span className="font-semibold">Surname:&nbsp;</span>
+              <p>Doe</p>
+            </div>
+          </div>
+          <div className="lg:hidden sm:col-span-8 col-span-4 w-full flex flex-col justify-center items-center">
+            <div className="flex text-base">
+              <span className="font-semibold">Name:&nbsp;</span>
+              <p>John</p>
+            </div>
+            <div className="flex text-base">
+              <span className="font-semibold">Surname:&nbsp;</span>
+              <p>Doe</p>
+            </div>
+          </div>
+        </Grid>
       </div>
     </Container>
   );
